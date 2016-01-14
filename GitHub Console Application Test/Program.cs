401407ceb3163c5b6ec946a1cafba3464a76f37c
Ammar <ammarhassan48@gmail.com>
2016-01-14 07:36:43 +0500

    update from ammar

---
+++ GitHub Console Application Test/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GitHub_Console_Application_Test
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello world");
+            Console.ReadLine();
+        }
+    }
+}
